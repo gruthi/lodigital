0707ee0472dcdcdd44a403074dcd4146f46b2505
gruthi <gruthi@gmail.com>
2020-01-17 10:31:36 +0200

    Initial commit
    login and register on server side

--- server/routeHelper.js
+++ server/routeHelper.js
@@ -3,28 +3,94 @@ const url = "mongodb://localhost:27017/";
 const myDb = "lodigitalDB";
 const usersColl = "users";
 
-function handleGet(req, res) {
-  console.log(req.query.userName);
-  //--------------------
+
+function login(req, res) {
+  // console.log(req.query.userName);
+  console.log('--0--');
   MongoClient.connect(url, function(err, db) {
     if (err) {
-      res.sendStatus(500);
-      return;
+      console.log('--1--');
+      return res.sendStatus(500);
+    }
+    const dbo = db.db(myDb);
+    //req.body =={ email: req.body.email,password: req.body.password }
+    dbo.collection(usersColl).findOne(req.body,
+        function(err, userFound) {
+          if (err) {
+            console.log('--2--');
+            return res.sendStatus(500);
+          }
+          if (!userFound) {
+            console.log('--3--');
+            return res.sendStatus(404);
+          }
+          return res.sendStatus(200);
+         
+  });
+});
+}
+function register(req, res) {
+  // console.log(req.query.userName);
+  console.log('--0--');
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log('--1--');
+      return res.sendStatus(500);
+    }
+    const dbo = db.db(myDb);
+    dbo.collection(usersColl).findOne({ email: req.body.email },
+        function(err, userFound) {
+          if (err) {
+            console.log('--2--');
+            return res.sendStatus(500);
+          }
+          if (userFound) {
+            console.log('--3--');
+            return res.sendStatus(400);
+          }
+          dbo.collection(usersColl).insertOne(req.body, function(err, result) {
+            if (err) {
+              console.log(err.message);
+              return res.sendStatus(500);
+            }       
+            return res.sendStatus(201);
+          
+        });
+  });
+});
+}
+function handleGet(req, res) {
+  console.log(req.query.userName);
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      return res.sendStatus(500);
     }
     let dbo = db.db(myDb);
-    let test = dbo
-      .collection(usersColl)
-      .find({ email: req.query.userName, password: req.query.passWord });
-    test.count().then(number => console.log("count: " + number));
-    if (test.count > 0) {
-    }
-    // console.log(test)
+    dbo.collection(usersColl).findOne({ email: req.query.email, password: req.query.passWord },
+        function(err, result) {
+          if (err) {
+            // throw err;
+            res.sendStatus(403);
+            return;
+          }
+          if (result) {
+            console.log('--1--');
+            res.sendStatus(200);
+            return;
+          } else {
+            console.log('--2--');
+            res.sendStatus(401);
+            return;
+          }
+          // db.close();
+        }
+      );
   });
-
-  //--------------------
-  res.send(req.body);
 }
 module.exports.handleGet = handleGet;
+module.exports.register = register;
+module.exports.login = login;
 ////////////////////////////
 //
 // dbo.createCollection(usersColl, function(err, res) {
