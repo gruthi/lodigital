8012dbda029c6bb762ea8ddd8e1df87426b52da6
henilana <henilana@gmail.com>
2020-01-19 20:20:16 +0200

    style

---
+++ server/client/src/pages/AboutTheCourse.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./PageTemplate.css";
+
+function Syllabus() {
+    // const content = "hello";
+
+
+    
+    return (
+        <div className="AboutTheVenture PageTemplate">
+            {/* <PageTemplate>  */}
+            <div>
+      <h1>AboutTheCourse</h1>
+      <p>
+      <h2>h2</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p>
+      <h2>h2</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p>
+      <h2>h2</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p>
+      <h2>h2</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+    {/* </PageTemplate>  */}
+        </div>
+    );
+};
+
+
+export default Syllabus;
