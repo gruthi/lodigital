f8adf8a5e0fe3ec585efe1260e1c0b39211468cd
henilana <henilana@gmail.com>
2020-01-22 00:55:34 +0200

    better style page template

--- server/client/src/pages/TitlePage.js
+++ server/client/src/pages/TitlePage.js
@@ -1,19 +1,37 @@
 import React from "react";
+import "./TitlePage.css";
 
 function TitlePage (props) {
-    if(props.title){
-        return(
-            <div className="TitlePage">
-                <h1>{props.title}</h1><h1>{props.title}</h1>
+    const myProps =  props.title || props.subTitle ;
+    // if(props.title){
+
+    //     return(
+    //         <div className="TitlePage">
+    //             <h1>{props.title}</h1><h1>{props.title}</h1>
+    //         </div>
+    //     );
+    // }else{
+    //     return(
+    //         <div className="SubTitlePage">
+    //             <h2>{props.subTitle}</h2><h2>{props.subTitle}</h2>
+    //         </div>
+    //     ); 
+    // }
+    
+        // return(
+        //     <div className={props.title? "TitlePage" : "SubTitlePage"}>
+        //         <h1>{ props.title | props.subTitle }</h1>
+        //         <h1>{ props.title | props.subTitle }</h1>
+        //     </div>
+        // );
+    
+   return(
+            <div className={props.title? "TitlePage" : "SubTitlePage"}>
+                <h1>{ myProps}</h1>
+                <h1>{ myProps }</h1>
             </div>
         );
-    }else{
-        return(
-            <div className="SubTitlePage">
-                <h2>{props.subTitle}</h2><h2>{props.subTitle}</h2>
-            </div>
-        ); 
-    }
+    
 }
 
 export default TitlePage;
