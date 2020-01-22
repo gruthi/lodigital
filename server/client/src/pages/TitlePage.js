af10f1c03a28aecc9e3a9f8914ec774f0fb002a0
henilana <henilana@gmail.com>
2020-01-22 10:40:23 +0200

    fix to short code

--- server/client/src/pages/TitlePage.js
+++ server/client/src/pages/TitlePage.js
@@ -3,35 +3,16 @@ import "./TitlePage.css";
 
 function TitlePage (props) {
     const myProps =  props.title || props.subTitle ;
-    // if(props.title){
 
-    //     return(
-    //         <div className="TitlePage">
-    //             <h1>{props.title}</h1><h1>{props.title}</h1>
-    //         </div>
-    //     );
-    // }else{
-    //     return(
-    //         <div className="SubTitlePage">
-    //             <h2>{props.subTitle}</h2><h2>{props.subTitle}</h2>
-    //         </div>
-    //     ); 
-    // }
-    
-        // return(
-        //     <div className={props.title? "TitlePage" : "SubTitlePage"}>
-        //         <h1>{ props.title | props.subTitle }</h1>
-        //         <h1>{ props.title | props.subTitle }</h1>
-        //     </div>
-        // );
-    
    return(
-            <div className={props.title? "TitlePage" : "SubTitlePage"}>
-                <h1>{ myProps}</h1>
-                <h1>{ myProps }</h1>
-            </div>
-        );
-    
+        <div className={props.title ? "TitlePage" : "SubTitlePage"}>
+            {props.title ? 
+                (<div><h1>{myProps}</h1><h1>{myProps}</h1></div>)
+                :
+                (<div><h2>{myProps}</h2><h2>{myProps}</h2></div>)
+            }
+        </div>
+    );
 }
 
 export default TitlePage;
