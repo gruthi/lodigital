e8c686b8c254ee628b0011bede31779c6328440d
henilana <henilana@gmail.com>
2020-03-02 15:31:59 +0200

    handle footer

--- server/client/src/Footer.js
+++ server/client/src/Footer.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./Footer.css";
-// import { FaFacebook } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 
 function Footer(){
    
   return (
-    <div className="Footer container">
-       {/* <a href='https://facebook.com/lodigital2019'> <FaFacebook size={30}/></a>    
-      <span size={70}>האתר נבנה בידי בוגרות הקורס</span> */}
+    <div className="Footer">
+      {/* <div className="Footer container"></div> */}
+      <div className="Footer container-fluid">
+      <div className="row">
+      {/* <div className="Footer container-fluid list-unstyled list-inline"> */}
+       {/* <a href='https://facebook.com/lodigital2019' className="list-inline-item">  */}
+       {/* <div  className="col bg-primary"></div>  */}
+       
+       <div className="col-3">
+         <a href='https://facebook.com/lodigital2019' > 
+        <FaFacebook />
+       </a></div>
+       <div  className="col-6 text-center">
+         האתר נבנה בידי בוגרות הקורס
+         </div>  
+      </div>
+      </div>
     </div>
   );
 };
