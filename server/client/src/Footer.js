be2620af96e99c83df8c76b5bdc412a68dd6a693
henilana <henilana@gmail.com>
2020-03-03 09:37:43 +0200

    fix style footer

--- server/client/src/Footer.js
+++ server/client/src/Footer.js
@@ -6,24 +6,19 @@ import { FaFacebook } from "react-icons/fa";
 function Footer(){
    
   return (
-    <div className="Footer">
-      {/* <div className="Footer container"></div> */}
       <div className="Footer container-fluid">
-      <div className="row">
-      {/* <div className="Footer container-fluid list-unstyled list-inline"> */}
-       {/* <a href='https://facebook.com/lodigital2019' className="list-inline-item">  */}
-       {/* <div  className="col bg-primary"></div>  */}
-       
-       <div className="col-3">
-         <a href='https://facebook.com/lodigital2019' > 
-        <FaFacebook />
-       </a></div>
-       <div  className="col-6 text-center">
-         האתר נבנה בידי בוגרות הקורס
-         </div>  
-      </div>
+        <div className="row">
+          <div className="col-3">
+            <a href='https://facebook.com/lodigital2019' > 
+              <FaFacebook />
+            </a>
+          </div>
+          <div  className="col-6 text-center">
+            האתר נבנה בידי בוגרות הקורס
+          </div>  
       </div>
     </div>
+    
   );
 };
 
