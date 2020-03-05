4eb6cdd7f699dc3f0b2470308d81761d9c4c7593
henilana <henilana@gmail.com>
2020-03-05 10:28:16 +0200

    proofreading

--- server/client/src/pages/Projects.js
+++ server/client/src/pages/Projects.js
@@ -6,12 +6,12 @@ function Projects() {
   let txt = [
     // {subTitle:'',subTxt:''},
   {subTitle:'פרויקט סיום פנימי',subTxt:'פרויקט סופי FullStack על פי נושאים שיעלו הסטודנטים. ביצוע בזוגות.'},
-  {subTitle:'פרויקט סיום חיצוני -חניכה בשיתוף מעסיקים מתעשית ההי-טק',subTxt:'פרויקט סופי FullStack בקבוצות של עד 4 סטודנטים ,בשיתוף מעסיק.'}];
-  let lst=['פרויקטון עיצוב דף סטטי בסיום CSS, HTML',
+  {subTitle:'פרויקט סיום חיצוני - חניכה בשיתוף מעסיקים מתעשית ההי-טק',subTxt:'פרויקט סופי FullStack בקבוצות של עד 4 סטודנטים, בשיתוף מעסיק.'}];
+  let lst=['פרויקטון עיצוב דף סטטי בסיום Css, HTML',
 'פרויקטון תכנות בסיום JS',
 'פרויקטון Frontend בסיום React',
 'פרויקטון Server בסיום Node',
-'Mongo+Express בסיום Web Server פרויקט'
+'פרויקטון MongoDB+Express בסיום Web Server'
 ];
   return (
     <div className="pageTemplate backTemp">
--- server/client/src/pages/TechnologiesAreTaught.js
+++ server/client/src/pages/TechnologiesAreTaught.js
@@ -4,9 +4,9 @@ import TitlePage from "./TitlePage.js";
 
 function TechnologiesAreTaught() {
   var txt = [
-  {subTitle:'צד-לקוח',subTxt:'יצירת ממשקים בסיסיים ואחר כך גם מתקדמים ,מעוצבים ודינאמיים. בחלק זה נלמד  HTML,CSS,JAVASCRIPT,REACT. '},
-  {subTitle:'צד-שרת',subTxt:'סביבת ה Back-End הפופולרית לפיתוח Servers-Web כיום js.Node:Express.'},
-  {subTitle:'אחסון הנתונים',subTxt:'BIG DATA- אחסון מידע לא-מובנה המאפיין את עולם האינטרנט וה -MONGODB.'}
+  {subTitle:'צד-לקוח',subTxt:'יצירת ממשקים בסיסיים ואחר כך גם מתקדמים, מעוצבים ודינאמיים. בחלק זה נלמד: HTML, Css, Javascript, React.'},
+  {subTitle:'צד-שרת',subTxt:'נלמד את סביבת ה-Back-End הפופולרית לפיתוח Servers-Web כיום: Node.js:Express.'},
+  {subTitle:'אחסון הנתונים',subTxt:'BIG DATA - אחסון מידע לא-מובנה המאפיין את עולם האינטרנט וה -MongoDB.'}
 ]
   return (
     <div className="pageTemplate backTemp">
