289b1c423ec329c1cb9920dbff94ecff562d28b3
henilana <henilana@gmail.com>
2020-03-22 01:32:34 +0200

    logo link to homepage and another Repairs

--- server/client/src/HeaderPage.js
+++ server/client/src/HeaderPage.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./HeaderPage.css";
 import logo from "./images/lodigital-logo.png";
 
+import { Link } from "react-router-dom";
+
 function HeaderPage(){
    
   return (
     <div className="HeaderPage" expand="md">
       <div className="wrraper">
-        <img className="logoImg" src={logo} alt="Logo" />
+        <Link to="/" >
+          <img className="logoImg" src={logo} alt="Logo" />
+        </Link>
         <span />
       </div>
      
