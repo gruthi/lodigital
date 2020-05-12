d449c90aca81f249a10e86975da4015c830df11d
henilana <henilana@gmail.com>
2020-05-12 23:35:07 +0300

    Footer - Rephrasing & Adding Links & Designing

--- server/client/src/Footer.js
+++ server/client/src/Footer.js
@@ -7,16 +7,34 @@ function Footer(){
    
   return (
       <div className="Footer container-fluid">
-        <div className="row">
-          <div className="col-3">
+        {/* <div className="row"> */}
+          {/* <div className="col-sm-3">
             <a href='https://facebook.com/lodigital2019' > 
               <FaFacebook />
             </a>
           </div>
-          <div  className="col-6 text-center">
-            האתר נבנה בידי בוגרות הקורס
-          </div>  
-      </div>
+          <div  className="col-sm-6 text-center">
+            האתר נבנה בידי בוגרות הקורס רותי גליק & אילנה חן *
+          </div> 
+          <div className="col-sm-3 text-right">
+           אתר בהרצה * 
+          </div>  */}
+          <div className="to-left">
+            <a href='https://facebook.com/lodigital2019' target="_blank" rel="noopener noreferrer">
+              <FaFacebook />
+            </a>
+          </div>
+          <div className="to-right">
+          אתר בהרצה - נבנה על ידי 
+          <a href='https://www.linkedin.com/in/ruthi-glick-2a81a311' target="_blank" rel="noopener noreferrer"> רותי גליק </a>
+           & 
+           <a href='https://www.linkedin.com/in/ilana-hen-05300b198/' target="_blank" rel="noopener noreferrer"> אילנה חן </a>
+           , מבוגרי הקורס
+          </div> 
+          {/* <div className="text-right">
+           אתר בהרצה * 
+          </div>  */}
+      {/* </div> */}
     </div>
     
   );
